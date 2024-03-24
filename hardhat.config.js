5310de29c0d0e6ae5a32e268c39907059ce82220
laruh <oblomoff616@gmail.com>
2024-03-24 09:28:21 +0700

    configs to be able to connect etomic-swap project to Remix

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,9 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     hardhat: {
-      // Hardhat Network's default settings are fine for most projects
+      chainId: 1337, // or another number that Remix will accept
+      host: "0.0.0.0", // Listen on all network interfaces
+      port: 8545 // Ensure this matches the port exposed in docker-compose
     }
   }
 };
