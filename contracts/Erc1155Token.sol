006e6fd52334530f23624a2139d0eb5299c4cd10
laruh <oblomoff616@gmail.com>
2024-05-28 14:22:59 +0700

    implement pub mint functions and recover nft funcs

--- contracts/Erc1155Token.sol
+++ contracts/Erc1155Token.sol
@@ -2,11 +2,17 @@
 
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Erc1155Token is ERC1155 {
-    constructor(string memory tokenUri) ERC1155(tokenUri) {
+contract Erc1155Token is ERC1155, Ownable  {
+    constructor(string memory tokenUri) ERC1155(tokenUri) Ownable(msg.sender) {
         uint256 tokenId = 1;
         uint256 amount = 3;
         _mint(msg.sender, tokenId, amount, "");
+    }
+
+    // Public mint function
+    function mint(address to, uint256 tokenId, uint256 amount, bytes memory data) public onlyOwner {
+        _mint(to, tokenId, amount, data);
     }
 }
--- contracts/Erc721Token.sol
+++ contracts/Erc721Token.sol
@@ -2,12 +2,19 @@
 
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Erc721Token is ERC721 {
+contract Erc721Token is ERC721, Ownable {
     constructor(string memory tokenName, string memory tokenSymbol)
-    ERC721(tokenName, tokenSymbol)
+        ERC721(tokenName, tokenSymbol)
+        Ownable(msg.sender)  // Initialize Ownable with the deployer address
     {
         uint256 tokenId = 1;
         _mint(msg.sender, tokenId);
+    }
+
+    // Public mint function
+    function mint(address to, uint256 tokenId) public onlyOwner {
+        _mint(to, tokenId);
     }
 }
